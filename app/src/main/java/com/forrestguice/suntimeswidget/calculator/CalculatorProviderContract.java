5efea2555f143a1500498e5d986c34b360b9de5d
Forrest Guice <forrestguice@users.noreply.github.com>
2018-11-10 23:37:36 -0700

    SuntimesCalculator
    code migrated from Suntimes v0.9.4 (9be27cdba0e4433d2ca42bf2858e52daecdb569f)

---
+++ app/src/main/java/com/forrestguice/suntimeswidget/calculator/CalculatorProviderContract.java
@@ -0,0 +1,74 @@
+/**
+    Copyright (C) 2018 Forrest Guice
+    This file is part of SuntimesWidget.
+
+    SuntimesWidget is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    SuntimesWidget is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with SuntimesWidget.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+package com.forrestguice.suntimeswidget.calculator;
+
+/**
+ * CalculatorProviderContract
+ * @version 0.1.0
+ *
+ * Moon Phases
+ *   The following URIs are supported:
+ *       content://suntimeswiget.calculator.provider/moon/phases                     .. get upcoming moon phases
+ *       content://suntimeswiget.calculator.provider/moon/phases/[millis]            .. get upcoming moon phases after date (timestamp)
+ *       content://suntimeswiget.calculator.provider/moon/phases/[millis]-[millis]   .. get upcoming moon phases for range (timestamp)
+ *
+ *   The result will be one or more rows containing:
+ *       [COLUMN_MOON_NEW(long), COLUMN_MOON_FIRST(long), COLUMN_MOON_FULL(long), COLUMN_MOON_THIRD(long)]
+ *
+ *
+ * Solstice and Equinox
+ *   The following URIs are supported:
+ *       content://suntimeswidget.calculator.provider/seasons                         .. get vernal, summer, autumn, and winter dates for this year
+ *       content://suntimeswidget.calculator.provider/seasons/[year]                  .. get vernal, summer, autumn, and winter dates for some year
+ *       content://suntimeswidget.calculator.provider/seasons/[year]-[year]           .. get vernal, summer, autumn, and winter dates for range
+ *
+ *   The result will be one or more rows containing:
+ *       [COLUMN_YEAR(int), COLUMM_SEASON_VERNAL(long), COLUMN_SEASON_SUMMER(long), COLUMN_SEASON_AUTUMN(long), COLUMN_SEASON_WINTER(long)]
+ *
+ * ------------------------------------------------------------------------------------------------
+ * Example:
+ *     String[] projection = new String[] { SuntimesCalculatorProviderContract.COLUMN_MOON_NEW, SuntimesCalculatorProviderContract.COLUMN_MOON_FIRST, SuntimesCalculatorProviderContract.COLUMN_MOON_FULL, SuntimesCalculatorProviderContract.COLUMN_MOON_THIRD };*
+ *     Uri uri = Uri.parse("content://" + SuntimesCalculatorProviderContract.AUTHORITY + "/" + SuntimesCalculatorProviderContract.QUERY_MOONPHASE + "/" + startDate.getTimeInMillis() + "-" + endDate.getTimeInMillis());*
+ *     Cursor cursor = resolver.query(uri, projection, null, null, null);
+ */
+public final class CalculatorProviderContract
+{
+    public static final String AUTHORITY = "suntimeswidget.calculator.provider";
+
+    public static final String QUERY_CONFIG = "config";
+    public static final String COLUMN_CONFIG_APPTHEME = "apptheme";
+    public static final String COLUMN_CONFIG_LOCALE = "locale";
+    public static final String COLUMN_CONFIG_LATITUDE = "latitude";
+    public static final String COLUMN_CONFIG_LONGITUDE = "longitude";
+    public static final String COLUMN_CONFIG_ALTITUDE = "altitude";
+    public static final String COLUMN_CONFIG_TIMEZONE = "timezone";
+
+    public static final String QUERY_MOONPHASE = "moon/phases";
+    public static final String COLUMN_MOON_NEW = "new";
+    public static final String COLUMN_MOON_FIRST = "first";
+    public static final String COLUMN_MOON_FULL = "full";
+    public static final String COLUMN_MOON_THIRD = "third";
+
+    public static final String QUERY_SEASONS = "seasons";
+    public static final String COLUMN_SEASON_YEAR = "year";
+    public static final String COLUMN_SEASON_VERNAL = "vernal";
+    public static final String COLUMN_SEASON_SUMMER = "summer";
+    public static final String COLUMN_SEASON_AUTUMN = "autumn";
+    public static final String COLUMN_SEASON_WINTER = "winter";
+}
---
+++ app/src/main/java/com/forrestguice/suntimeswidget/calendar/SuntimesCalendarActivity.java
@@ -0,0 +1,296 @@
+/*
+    Copyright (C) 2018 Forrest Guice
+    This file is part of SuntimesWidget.
+
+    SuntimesWidget is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    SuntimesWidget is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with SuntimesWidget.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+package com.forrestguice.suntimeswidget.calendar;
+
+import android.Manifest;
+import android.annotation.TargetApi;
+import android.app.Activity;
+import android.app.AlertDialog;
+
+import android.content.ContentResolver;
+import android.content.Context;
+import android.content.ContextWrapper;
+import android.content.DialogInterface;
+import android.content.SharedPreferences;
+import android.content.pm.PackageManager;
+
+import android.content.res.Configuration;
+import android.content.res.Resources;
+import android.database.Cursor;
+import android.net.Uri;
+import android.os.Build;
+import android.os.Bundle;
+import android.preference.CheckBoxPreference;
+
+import android.preference.Preference;
+import android.preference.PreferenceFragment;
+import android.preference.PreferenceManager;
+
+import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
+import android.text.Html;
+import android.text.Spanned;
+import android.util.DisplayMetrics;
+import android.util.Log;
+
+import com.forrestguice.suntimescalendar.R;
+import com.forrestguice.suntimeswidget.calculator.CalculatorProviderContract;
+
+import java.util.Locale;
+
+/**
+ * SuntimesCalendarActivity
+ */
+public class SuntimesCalendarActivity extends Activity
+{
+    public static final String THEME_LIGHT = "light";
+    public static final String THEME_DARK = "dark";
+
+    public static final int REQUEST_CALENDARPREFSFRAGMENT_ENABLED = 2;
+    public static final int REQUEST_CALENDARPREFSFRAGMENT_DISABLED = 4;
+
+    private Context context;
+    private String config_apptheme = null;
+    private static String systemLocale = null;  // null until locale is overridden w/ loadLocale
+
+    public SuntimesCalendarActivity()
+    {
+        super();
+    }
+
+    ///////////////////////////////////////////////////////////////////////////////////////////////
+
+    @Override
+    protected void attachBaseContext(Context newBase)
+    {
+        ContentResolver resolver = newBase.getContentResolver();
+        if (resolver != null)
+        {
+            Uri uri = Uri.parse("content://" + CalculatorProviderContract.AUTHORITY + "/" + CalculatorProviderContract.QUERY_CONFIG );
+            String[] projection = new String[] { CalculatorProviderContract.COLUMN_CONFIG_LOCALE, CalculatorProviderContract.COLUMN_CONFIG_APPTHEME };
+            try {
+                Cursor cursor = resolver.query(uri, projection, null, null, null);
+                if (cursor != null)
+                {
+                    cursor.moveToFirst();
+                    String locale = cursor.getString(0);
+                    config_apptheme = cursor.getString(1);
+                    cursor.close();
+                    super.attachBaseContext((locale != null) ? loadLocale(newBase, locale) : resetLocale(newBase));
+
+                } else {
+                    super.attachBaseContext(newBase);
+                }
+            } catch (SecurityException e) {
+                Log.e("SuntimesCalendar", "attachBaseContext: Unable to access provider! " + e);
+                super.attachBaseContext(newBase);
+            }
+        } else super.attachBaseContext(newBase);
+    }
+
+    private static Context loadLocale( Context context, String languageTag )
+    {
+        if (systemLocale == null) {
+            systemLocale = Locale.getDefault().getLanguage();
+        }
+
+        Locale customLocale = localeForLanguageTag(languageTag);
+        Locale.setDefault(customLocale);
+        Log.i("loadLocale", languageTag);
+
+        Resources resources = context.getApplicationContext().getResources();
+        Configuration config = resources.getConfiguration();
+
+        if (Build.VERSION.SDK_INT >= 17)
+            config.setLocale(customLocale);
+        else config.locale = customLocale;
+
+        if (Build.VERSION.SDK_INT >= 25) {
+            return new ContextWrapper(context.createConfigurationContext(config));
+
+        } else {
+            DisplayMetrics metrics = resources.getDisplayMetrics();
+            //noinspection deprecation
+            resources.updateConfiguration(config, metrics);
+            return new ContextWrapper(context);
+        }
+    }
+
+    private static Context resetLocale( Context context )
+    {
+        if (systemLocale != null) {
+            return loadLocale(context, systemLocale);
+        }
+        return context;
+    }
+
+    private static @NonNull Locale localeForLanguageTag(@NonNull String languageTag)
+    {
+        Locale locale;
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP)
+        {
+            locale = Locale.forLanguageTag(languageTag.replaceAll("_", "-"));
+
+        } else {
+            String[] parts = languageTag.split("[_]");
+            String language = parts[0];
+            String country = (parts.length >= 2) ? parts[1] : null;
+            locale = (country != null) ? new Locale(language, country) : new Locale(language);
+        }
+        Log.d("localeForLanguageTag", "tag: " + languageTag + " :: locale: " + locale.toString());
+        return locale;
+    }
+
+    ///////////////////////////////////////////////////////////////////////////////////////////////
+
+    @Override
+    public void onCreate(Bundle icicle)
+    {
+        setResult(RESULT_OK);
+        context = this;
+
+        if (config_apptheme != null) {
+            setTheme(config_apptheme.equals(THEME_LIGHT) ? R.style.AppTheme_Light : R.style.AppTheme_Dark);
+        }
+
+        super.onCreate(icicle);
+        getFragmentManager().beginTransaction().replace(android.R.id.content, new CalendarPrefsFragment() ).commit();
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults)
+    {
+        if (grantResults.length > 0 && permissions.length > 0)
+        {
+            switch (requestCode)
+            {
+                case REQUEST_CALENDARPREFSFRAGMENT_ENABLED:
+                case REQUEST_CALENDARPREFSFRAGMENT_DISABLED:
+                    if (grantResults[0] == PackageManager.PERMISSION_GRANTED)
+                    {
+                        boolean enabled = requestCode == (REQUEST_CALENDARPREFSFRAGMENT_ENABLED);
+                        runCalendarTask(SuntimesCalendarActivity.this, enabled);
+
+                        SharedPreferences.Editor pref = PreferenceManager.getDefaultSharedPreferences(context).edit();
+                        pref.putBoolean(SuntimesCalendarSettings.PREF_KEY_CALENDARS_ENABLED, enabled);
+                        pref.apply();
+
+                        if (tmp_calendarPref != null)
+                        {
+                            tmp_calendarPref.setChecked(enabled);
+                            tmp_calendarPref = null;
+                        }
+                    }
+                    break;
+            }
+        }
+    }
+
+    /**
+    /**
+     * CalendarPrefsFragment
+     */
+    @TargetApi(Build.VERSION_CODES.HONEYCOMB)
+    public static class CalendarPrefsFragment extends PreferenceFragment
+    {
+        @Override
+        public void onCreate(Bundle savedInstanceState)
+        {
+            super.onCreate(savedInstanceState);
+            Log.i("CalendarPrefsFragment", "Arguments: " + getArguments());
+
+            PreferenceManager.setDefaultValues(getActivity(), R.xml.preference_calendars, false);
+            addPreferencesFromResource(R.xml.preference_calendars);
+
+            initPref_calendars(CalendarPrefsFragment.this);
+        }
+    }
+
+    @TargetApi(Build.VERSION_CODES.HONEYCOMB)
+    private static void initPref_calendars(PreferenceFragment fragment)
+    {
+        CheckBoxPreference calendarsEnabledPref = (CheckBoxPreference) fragment.findPreference(SuntimesCalendarSettings.PREF_KEY_CALENDARS_ENABLED);
+        initPref_calendars(fragment.getActivity(), calendarsEnabledPref);
+    }
+    private static void initPref_calendars(final Activity activity, final CheckBoxPreference enabledPref )
+    {
+        final Preference.OnPreferenceChangeListener onPreferenceChanged0 = new Preference.OnPreferenceChangeListener()
+        {
+            @Override
+            public boolean onPreferenceChange(Preference preference, Object newValue)
+            {
+                boolean enabled = (Boolean)newValue;
+                int calendarPermission = ActivityCompat.checkSelfPermission(activity, Manifest.permission.WRITE_CALENDAR);
+                if (calendarPermission != PackageManager.PERMISSION_GRANTED)
+                {
+                    final int requestCode = (enabled ? REQUEST_CALENDARPREFSFRAGMENT_ENABLED : REQUEST_CALENDARPREFSFRAGMENT_DISABLED);
+                    if (ActivityCompat.shouldShowRequestPermissionRationale(activity, Manifest.permission.WRITE_CALENDAR))
+                    {
+                        String permissionMessage = activity.getString(R.string.privacy_permission_calendar);
+                        AlertDialog.Builder builder = new AlertDialog.Builder(activity);
+                        builder.setTitle(activity.getString(R.string.privacy_permissiondialog_title))
+                                .setMessage(fromHtml(permissionMessage))
+                                .setPositiveButton(android.R.string.yes, new DialogInterface.OnClickListener()
+                                {
+                                    public void onClick(DialogInterface dialog, int which)
+                                    {
+                                        ActivityCompat.requestPermissions(activity, new String[] { Manifest.permission.WRITE_CALENDAR }, requestCode);
+                                        tmp_calendarPref = enabledPref;
+                                    }
+                                });
+
+                        //if (Build.VERSION.SDK_INT >= 11)
+                            //builder.setIconAttribute(R.attr.icActionWarning);
+                        //else builder.setIcon(R.drawable.ic_action_warning);
+
+                        builder.show();
+                        return false;
+
+                    } else {
+                        ActivityCompat.requestPermissions(activity, new String[] { Manifest.permission.WRITE_CALENDAR }, requestCode);
+                        tmp_calendarPref = enabledPref;
+                        return false;
+                    }
+
+                } else {
+                    runCalendarTask(activity, enabled);
+                    return true;
+                }
+            }
+        };
+        enabledPref.setOnPreferenceChangeListener(onPreferenceChanged0);
+    }
+    private static CheckBoxPreference tmp_calendarPref = null;
+
+    private static void runCalendarTask(final Activity activity, boolean enabled)
+    {
+        SuntimesCalendarTask calendarTask = new SuntimesCalendarTask(activity);
+        if (!enabled) {
+            calendarTask.setFlagClearCalendars(true);
+        }
+        calendarTask.execute();
+    }
+
+    public static Spanned fromHtml(String htmlString )
+    {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N)
+            return Html.fromHtml(htmlString, Html.FROM_HTML_MODE_LEGACY);
+        else return Html.fromHtml(htmlString);
+    }
+}
