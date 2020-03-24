d4c91fcb7f0a7ac11b1fd43a3a9908f186759a20
Forrest Guice <forrestguice@users.noreply.github.com>
2020-03-23 21:11:38 -0700

    factory

---
+++ app/src/main/java/com/forrestguice/suntimeswidget/calendar/task/SuntimesCalendarDescriptor.java
@@ -0,0 +1,154 @@
+/**
+    Copyright (C) 2020 Forrest Guice
+    This file is part of SuntimesCalendars.
+
+    SuntimesCalendars is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    SuntimesCalendars is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with SuntimesCalendars.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+package com.forrestguice.suntimeswidget.calendar.task;
+
+import android.content.Context;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.content.pm.ResolveInfo;
+import android.support.annotation.NonNull;
+import android.util.Log;
+
+import java.util.HashMap;
+import java.util.List;
+
+@SuppressWarnings("Convert2Diamond")
+public class SuntimesCalendarDescriptor implements Comparable
+{
+    public SuntimesCalendarDescriptor(String name, String title, String summary, int color, String classRef)
+    {
+        calendarName = name;
+        calendarTitle = title;
+        calendarSummary = summary;
+        calendarColor = color;
+        calendarRef = classRef;
+    }
+
+    protected String calendarRef;
+    public String calendarRef() {
+        return calendarRef;
+    }
+
+    protected String calendarName;
+    public String calendarName() {
+        return calendarName;
+    }
+
+    protected String calendarTitle;
+    public String calendarTitle() {
+        return calendarTitle;
+    }
+
+    protected String calendarSummary;
+    public String calendarSummary() {
+        return calendarSummary;
+    }
+
+    protected int calendarColor;
+    public int calendarColor() {
+        return calendarColor;
+    }
+
+    protected boolean isPlugin = false;
+    public boolean isPlugin()
+    {
+        return isPlugin;
+    }
+    public void setIsPlugin( boolean value )
+    {
+        isPlugin = value;
+    }
+
+    @Override
+    public boolean equals(Object other)
+    {
+        if (!(other instanceof SuntimesCalendarDescriptor)) {
+            return false;
+        } else {
+            SuntimesCalendarDescriptor otherDescriptor = (SuntimesCalendarDescriptor) other;
+            return this.calendarName().equals(otherDescriptor.calendarName());
+        }
+    }
+
+    @Override
+    public int compareTo(@NonNull Object other) {
+        SuntimesCalendarDescriptor otherDescriptor = (SuntimesCalendarDescriptor)other;
+        return this.calendarName().compareTo(otherDescriptor.calendarName());
+    }
+
+    ////////////////////////////////////////////////////////////////////////////////////////////////
+    ////////////////////////////////////////////////////////////////////////////////////////////////
+
+    public static final String CATEGORY_SUNTIMES_CALENDAR = "com.forrestguice.suntimeswidget.SUNTIMES_CALENDAR";
+
+    public static final String KEY_NAME = "CalendarName";
+    public static final String KEY_TITLE = "CalendarTitle";
+    public static final String KEY_SUMMARY = "CalendarSummary";
+    public static final String KEY_COLOR = "CalendarColor";
+    public static final String KEY_REFERENCE = "CalendarReference";
+
+    protected static HashMap<String, SuntimesCalendarDescriptor> calendars = new HashMap<>();
+    protected static boolean initialized = false;
+
+    public static void initDescriptors(Context context)
+    {
+        PackageManager packageManager = context.getPackageManager();
+        Intent packageQuery = new Intent(Intent.ACTION_RUN);    // get a list of installed plugins
+        packageQuery.addCategory(CATEGORY_SUNTIMES_CALENDAR);
+        List<ResolveInfo> packages = packageManager.queryIntentActivities(packageQuery, PackageManager.GET_META_DATA);
+        Log.i("initDescriptors", "Scanning for calculator plugins... found " + packages.size());
+
+        for (ResolveInfo packageInfo : packages)
+        {
+            if (packageInfo.activityInfo != null && packageInfo.activityInfo.metaData != null)
+            {
+                String calendarName = packageInfo.activityInfo.metaData.getString(KEY_NAME);
+                String calendarTitle = packageInfo.activityInfo.metaData.getString(KEY_TITLE);
+                String calendarSummary = packageInfo.activityInfo.metaData.getString(KEY_SUMMARY);
+                int calendarColor = packageInfo.activityInfo.metaData.getInt(KEY_COLOR);
+                String calendarReference = packageInfo.activityInfo.metaData.getString(KEY_REFERENCE);
+
+                SuntimesCalendarDescriptor descriptor = new SuntimesCalendarDescriptor(calendarName, calendarTitle, calendarSummary, calendarColor, calendarReference);
+                descriptor.setIsPlugin(true);
+                SuntimesCalendarDescriptor.addValue(descriptor);
+                Log.i("initDescriptors", "..initialized plugin: " + descriptor.toString());
+            }
+        }
+        initialized = true;
+    }
+
+    public static void addValue( SuntimesCalendarDescriptor calendar )
+    {
+        if (!calendars.containsKey(calendar.calendarName())) {
+            calendars.put(calendar.calendarName(), calendar);
+        }
+    }
+
+    public static void removeValue( SuntimesCalendarDescriptor calendar ) {
+        calendars.remove(calendar.calendarName);
+    }
+
+    public static HashMap<String, SuntimesCalendarDescriptor> values(Context context)
+    {
+        if (!initialized) {
+            initDescriptors(context);
+        }
+        return new HashMap<>(calendars);
+    }
+}
---
+++ app/src/main/java/com/forrestguice/suntimeswidget/calendar/task/SuntimesCalendarFactory.java
@@ -0,0 +1,45 @@
+/**
+    Copyright (C) 2020 Forrest Guice
+    This file is part of SuntimesCalendars.
+
+    SuntimesCalendars is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    SuntimesCalendars is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with SuntimesCalendars.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+package com.forrestguice.suntimeswidget.calendar.task;
+
+import android.content.Context;
+import android.util.Log;
+
+@SuppressWarnings("Convert2Diamond")
+public class SuntimesCalendarFactory
+{
+    public SuntimesCalendar createCalendar(Context context, SuntimesCalendarDescriptor descriptor)
+    {
+        SuntimesCalendar calendar = null;
+        Class calendarClass = null;
+        try {
+            calendarClass = Class.forName(descriptor.calendarRef());
+            calendar = (SuntimesCalendar) calendarClass.newInstance();
+            calendar.init(context);
+
+        } catch (ClassNotFoundException e) {
+            Log.e(getClass().getSimpleName(), "Failed to createCalendar! " + e);
+        } catch (IllegalAccessException e) {
+            Log.e(getClass().getSimpleName(), "Failed to createCalendar! " + e);
+        } catch (InstantiationException e) {
+            Log.e(getClass().getSimpleName(), "Failed to createCalendar! " + e);
+        }
+        return calendar;
+    }
+}
